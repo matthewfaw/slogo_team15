072b71b5274b104090e1b8baa7acc8cab1eb34e1
Kayla Schulz <kms114@duke.edu>
2016-10-18 02:54:58 -0400

    Beginning of variable viewer

--- src/appScene/variableViewer/ConcreteVariableViewer.java
+++ src/appScene/variableViewer/ConcreteVariableViewer.java
@@ -2,9 +2,24 @@ package appScene.variableViewer;
 
 import java.util.Map;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class ConcreteVariableViewer implements IVariableViewer {
 
+    private BorderPane myVariableViewer;
+    
+    public ConcreteVariableViewer(int width, int height) {
+        myVariableViewer = new BorderPane();
+        VBox varNames = new VBox(width/2);
+        VBox varValues = new VBox(width/2);
+        myVariableViewer.setLeft(varNames);
+        myVariableViewer.setRight(varValues);
+        TextField curTextField = new TextField();
+        varNames.getChildren().add(curTextField);
+    }
+    
     @Override
     public void clear () {
         // TODO Auto-generated method stub
@@ -19,8 +34,7 @@ public class ConcreteVariableViewer implements IVariableViewer {
 
     @Override
     public Node getInstanceAsNode () {
-        // TODO Auto-generated method stub
-        return null;
+        return myVariableViewer;
     }
 
 }
--- src/applicationController/ApplicationController.java
+++ src/applicationController/ApplicationController.java
@@ -4,10 +4,9 @@ import appScene.errorViewer.ConcreteErrorViewer;
 import appScene.textEditor.ConcreteTextEditor;
 import appScene.toolbar.ConcreteToolbar;
 import appScene.turtleBox.ConcreteTurtleBox;
-import appScene.turtleBox.ITurtleBox;
+import appScene.variableViewer.ConcreteVariableViewer;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
@@ -21,6 +20,7 @@ public class ApplicationController {
    private ConcreteTextEditor myTextEditor;
    private ConcreteErrorViewer myErrorViewer;
    private ConcreteTurtleBox myTurtleBox;
+   private ConcreteVariableViewer myVariableViewer;
    
    public ApplicationController() {
        myApplicationView = new GridPane();
@@ -31,6 +31,7 @@ public class ApplicationController {
        myTextEditor = new ConcreteTextEditor( width/4, height );
        myErrorViewer = new ConcreteErrorViewer( size/2 , height/2 );
        myTurtleBox = new ConcreteTurtleBox( size, size );
+       myVariableViewer = new ConcreteVariableViewer(size/2, size/2);
        
        myRoot = new Group();
        myRoot.getChildren().addAll(myApplicationView);
@@ -39,6 +40,7 @@ public class ApplicationController {
        myApplicationView.add( myTextEditor.getInstanceAsNode(),  0, 1, 1, 2);
        myApplicationView.add( myTurtleBox.getInstanceAsNode(),	 1, 1, 2, 2);
        myApplicationView.add( myErrorViewer.getInstanceAsNode(), 3, 1, 1, 1);
+       myApplicationView.add(myVariableViewer.getInstanceAsNode(), 3, 2);
        return myScene;
    }
    
