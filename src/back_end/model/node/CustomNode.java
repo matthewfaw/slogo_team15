91bd3aace773b27cda1ce02ae6625ca1459a2f3f
Matthew Faw <mcf28@duke.edu>
2016-10-24 05:35:32 -0400

    Added the needed additions to AST to handle custom methods, and created new Node classes to deal with them.  Committing to switch to the extensions branch and get Hannah's changes to remove the remaining errors in my code

--- src/back_end/model/node/CustomNode.java
+++ src/back_end/model/node/CustomNode.java
@@ -1,20 +1,45 @@
 package back_end.model.node;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import back_end.model.exception.ArgumentException;
+import back_end.model.command.ICommandBranch;
 
 public class CustomNode extends Node {
+	private ICommandBranch myMethod;
+	
+	private ArrayList<Node> myInputs;
+	private ArrayList<Node> myFunction;
+	
+	private int myReturnValue;
+	private NodeState myEvaluationState;
 
-	public CustomNode()
+	public CustomNode(ICommandBranch aCommand, int aNumberOfInputs)
 	{
 		super();
+		
+		myEvaluationState = NodeState.EVALUATING_INPUTS;
+		
+		myMethod = aCommand;
 	}
 
 	@Override
 	public double eval() throws ArgumentException {
-		// TODO Auto-generated method stub
-		return 0;
+		Node[] inputs = myFunction.toArray(new Node[myFunction.size()]);
+		myReturnValue = myMethod.eval(inputs);
+		myMethod.resetScope();
+		
+		return myReturnValue;
+	}
+	public void evalInputs()
+	{
+		myMethod.setScope();
+
+		Node[] myInputList = myInputs.toArray(new Node[myInputs.size()]);
+		myMethod.eval(myInputList);
+		
+		myFunction = myMethod.getFunction().getChildren();
 	}
 
 	@Override
@@ -24,6 +49,52 @@ public class CustomNode extends Node {
 
 	@Override
 	public double getValue() {
-		return 0;
+		return myReturnValue;
 	}
+	
+	public void addChildInputs(List<Node> aInputs)
+	{
+		myInputs = aInputs;
+	}
+	public List<Node> getChildInputs()
+	{
+		return myInputs;
+	}
+	
+	public List<Node> getFunction()
+	{
+		return myFunction;
+	}
+	
+	public void setEvaluationState(NodeState aState)
+	{
+		myEvaluationState = aState;
+	}
+	public NodeState getEvaluationState()
+	{
+		return myEvaluationState;
+	}
+	//XXX: combine with BranchNode since similar
+	public void unmarkCustomChildren()
+	{
+		//Unmark all conditions
+		for (Node child: myFunction) {
+			child.setState(NodeState.AVAILABLE);
+			unmarkCustomChildren(child);
+		}
+	}
+	
+	//XXX: combine with BranchNode since similar
+	private void unmarkCustomChildren(Node aParent)
+	{
+		if (aParent instanceof CustomNode) {
+			((CustomNode) aParent).unmarkCustomChildren();
+		} else {
+			for (Node child: aParent.getChildren()) {
+				child.setState(NodeState.AVAILABLE);
+				unmarkCustomChildren(child);
+			}
+		}
+	}
+	
 }
---
+++ src/back_end/model/node/ToNode.java
@@ -0,0 +1,49 @@
+package back_end.model.node;
+
+import java.util.ArrayList;
+
+import back_end.model.command.ICommand;
+import back_end.model.exception.ArgumentException;
+
+public class ToNode extends Node {
+	private ICommand myMethod;
+	private int myNumberOfInputs;
+	
+	private VariableNode myNameNode;
+	private	ArrayList<VariableNode> myInputVariables;
+	private ListNode myCustomMethod;
+	
+	private double returnValue;
+	
+	public ToNode(ICommand aMethod, int aNumberOfInputs)
+	{
+		myMethod = aMethod;
+		myNumberOfInputs = aNumberOfInputs;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return returnValue;
+	}
+
+	@Override
+	public double eval() throws ArgumentException {
+		ArrayList<Node> allInputs = new ArrayList<Node>(myInputVariables);
+		allInputs.add(0, myNameNode);
+		allInputs.add(1, myCustomMethod);
+		
+		Node[] allInputsArray = allInputs.toArray(new Node[allInputs.size()]);
+
+		returnValue = myMethod.eval(allInputsArray);
+		
+		return returnValue;
+	}
+
+}
