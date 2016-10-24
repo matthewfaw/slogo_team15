dfb311987c486b3cbe6356686f72f8858db2d30f
Kayla Schulz <kms114@duke.edu>
2016-10-24 03:08:18 -0400

    Canvas now works and beginning of turtle movement

--- src/front_end/appScene/turtleBox/ConcreteTurtleBox.java
+++ src/front_end/appScene/turtleBox/ConcreteTurtleBox.java
@@ -3,7 +3,10 @@ package front_end.appScene.turtleBox;
 import back_end.model.robot.IViewRobot;
 import integration.languages.Languages;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -12,6 +15,8 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 class ConcreteTurtleBox implements ITurtleBox {
 
@@ -20,23 +25,46 @@ class ConcreteTurtleBox implements ITurtleBox {
 	ColorPicker myBackgroundColorPicker;
 	IViewRobot myRobot;
 	ImageView myTurtle;
+	Group root;
+	Canvas drawingCanvas;
+	GraphicsContext gc;
+
 	
 	private final int CHARACTER_SIZE = 50;
 	
 	ConcreteTurtleBox(int width, int height){
+	        root = new Group();
+	        drawingCanvas = new Canvas(width, height);
+	        gc = drawingCanvas.getGraphicsContext2D();
 		myScroller = new ScrollPane();
 		myScroller.setPrefSize(width, height);
 		myScroller.setMinSize(width, height);
 		myScroller.setMaxSize(width, height);
 		
 		mySandbox = new Pane();
-		mySandbox.setPrefSize(width, height);
-		mySandbox.setMinSize(width, height);
-		mySandbox.setMaxSize(width, height);
-		
-		myScroller.setContent(mySandbox);
+                mySandbox.setPrefSize(width, height);
+                mySandbox.setMinSize(width, height);
+                mySandbox.setMaxSize(width, height);
 
-		initBox(width, height);
+		myScroller.setContent(mySandbox);
+                testDraw(gc);
+                initBox(width, height);
+		
+	}
+	
+	private void testDraw(GraphicsContext gc) {
+	        gc.setStroke(Color.BLUE);
+	        gc.setLineWidth(5);
+	        gc.strokeLine(100, 120, 120, 240);
+	        mySandbox.getChildren().add(drawingCanvas);
+	}
+	
+	public GraphicsContext getGC() {
+	    return gc;
+	}
+	
+	public ImageView getTurtle() {
+	    return myTurtle;
 	}
 	
 	public void initBox(int width, int height) {
@@ -81,12 +109,12 @@ class ConcreteTurtleBox implements ITurtleBox {
 	
 	private void initColorPicker(){
 		myBackgroundColorPicker = new ColorPicker();
-		myBackgroundColorPicker.setOnAction(
-				e -> mySandbox.setBackground( new Background( new BackgroundFill(
-										myBackgroundColorPicker.getValue(),
-										CornerRadii.EMPTY, 
-										Insets.EMPTY
-										))));
+                myBackgroundColorPicker.setOnAction(
+                                                    e -> mySandbox.setBackground( new Background( new BackgroundFill(
+                                                                                                    myBackgroundColorPicker.getValue(),
+                                                                                                    CornerRadii.EMPTY, 
+                                                                                                    Insets.EMPTY
+                                                                                                    ))));
 		mySandbox.getChildren().add(myBackgroundColorPicker);
 	}
 
--- src/front_end/appScene/turtleBox/TurtleMovement.java
+++ src/front_end/appScene/turtleBox/TurtleMovement.java
@@ -1,7 +1,31 @@
 package front_end.appScene.turtleBox;
 
+import back_end.model.robot.IViewRobot;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
 public class TurtleMovement implements ITurtleMovement {
 
+    IViewRobot myRobot;
+    //TODO: Get rid of magic numbers here
+    ConcreteTurtleBox myConcreteTurtleBox = new ConcreteTurtleBox(500, 500);
+    private GraphicsContext gc;
+    private ImageView myTurtle;
+    private double curX = myRobot.getX();
+    private double curY = myRobot.getY();
     
+    public void updateTurtle() {
+        gc = myConcreteTurtleBox.getGC();
+        myTurtle = myConcreteTurtleBox.getTurtle();
+        if(myRobot.isPenDown() && curX != myRobot.getX()) {
+            for(int i = 0; i < Math.abs(curX-myRobot.getX()); i++) {
+                gc.setStroke(Color.BLACK);
+                gc.setLineWidth(5);
+                gc.strokeLine(curX, curX+i, myRobot.getY(), myRobot.getY());
+                myTurtle.setX(curX+i);
+            }
+        }
+    }
     
 }
