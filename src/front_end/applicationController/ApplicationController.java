ef45fa191439c59314db462de8e589bcbd1ecc4e
Kayla Schulz <kms114@duke.edu>
2016-10-28 14:30:09 -0400

    Addition of comboBox creator in pen popup, and clear works for selecting the pen color

--- src/front_end/applicationController/ApplicationController.java
+++ src/front_end/applicationController/ApplicationController.java
@@ -67,7 +67,6 @@ public class ApplicationController {
         myTurtleBox = myAppScene.getMyTurtleBox();
         myScriptViewer = myAppScene.getMyScriptViewer();
         myShapeColorModule = myAppScene.getMyShapeColorModule();
-        // TODO: Change this to interface - Kayla
         myPenPopup = new PenPopup();
     }
 
--- src/front_end/view_modules/penProperties/PenPopup.java
+++ src/front_end/view_modules/penProperties/PenPopup.java
@@ -1,16 +1,20 @@
 package front_end.view_modules.penProperties;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+
 
 /**
  * 
@@ -26,57 +30,74 @@ public class PenPopup implements IPenPopup {
     private Button applyButton;
     private Button clearButton;
     private Scene myScene;
-    
+    private ColorPicker myColorPicker;
+    private HBox testBox;
+
     @Override
-    public void initPopup(){
+    public void initPopup () {
         myRow = new HBox(SPACING);
         layout = new BorderPane();
         layout.setStyle("-fx-background-color: paleturquoise;");
         layout.setPrefSize(POPUP_SIZE, POPUP_SIZE);
         makeColorPicker();
         createEndingButtons();
+        setBorderPane();
         myScene = new Scene(layout);
     }
-    
+
     @Override
-    public Scene getScene() {
+    public Scene getScene () {
         return myScene;
     }
-    
-    private void makeColorPicker() {
+
+    private void setBorderPane () {
+        layout.setTop(myRow);
+        layout.setBottom(testBox);
+        layout.setCenter(createComboBox("hi", new ArrayList<String>()));
+    }
+
+    private void makeColorPicker () {
         Label penColorLabel = new Label("Choose your pen color: ");
-        ColorPicker myColorPicker = new ColorPicker(Color.WHITE);
+        myColorPicker = new ColorPicker(Color.WHITE);
         myRow.setPadding(new Insets(5, 20, 10, 20));
         myRow.getChildren().addAll(penColorLabel, myColorPicker);
-        layout.setTop(myRow);
     }
-    
-    private void createEndingButtons() {
+
+    private void createEndingButtons () {
         applyButton = makeButton("Apply Changes");
         clearButton = makeButton("Clear");
-        HBox testBox = new HBox(SPACING);
+        testBox = new HBox(SPACING);
         testBox.setPadding(new Insets(0, 20, 10, 20));
         testBox.setAlignment(Pos.CENTER_RIGHT);
         testBox.getChildren().addAll(clearButton, applyButton);
-        layout.setBottom(testBox);
+        
     }
-    
-    private Button makeButton(String buttonText) {
+
+    private HBox createComboBox (String labelName, List<String> myOptions) {
+        HBox comboBoxRow = new HBox(SPACING);
+        Label comboBoxLabel = new Label(labelName);
+        ComboBox<String> addComboBox = new ComboBox<String>();
+        addComboBox.getItems().addAll(myOptions);
+        comboBoxRow.getChildren().addAll(comboBoxLabel, addComboBox);
+        return comboBoxRow;
+    }
+
+    private Button makeButton (String buttonText) {
         Button addButton = new Button(buttonText);
         return addButton;
     }
-    
+
     @Override
-    public void clear() {
-        layout.getChildren().clear();
-        initPopup();
+    public void clear () {
+        myRow.getChildren().clear();
+        makeColorPicker();
     }
-    
+
     @Override
     public void onApplyPress (EventHandler<MouseEvent> aEvent) {
         applyButton.setOnMouseClicked(aEvent);
     }
-    
+
     @Override
     public void onClearPress (EventHandler<MouseEvent> aEvent) {
         clearButton.setOnMouseClicked(aEvent);
