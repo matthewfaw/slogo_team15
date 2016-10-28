4ebacfc95aecc5fab7a6448070bf443d0125b877
Kayla Schulz <kms114@duke.edu>
2016-10-28 01:55:40 -0400

    Addition of apply and clear buttons for pen properties popup

--- src/front_end/view_modules/penProperties/PenPopup.java
+++ src/front_end/view_modules/penProperties/PenPopup.java
@@ -1,10 +1,13 @@
 package front_end.view_modules.penProperties;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 
 /**
  * 
@@ -13,17 +16,18 @@ import javafx.scene.layout.Pane;
  */
 public class PenPopup {
 
-    private Pane layout;
+    private BorderPane layout;
     private HBox myRow;
     private static final int POPUP_SIZE = 600;
+    private static final int SPACING = 10;
     
     public Scene initPopup(){
-        myRow = new HBox();
-        //TODO: Should this be a border or stack pane?
-        layout = new Pane();
+        myRow = new HBox(SPACING);
+        layout = new BorderPane();
         layout.setStyle("-fx-background-color: paleturquoise;");
         layout.setPrefSize(POPUP_SIZE, POPUP_SIZE);
         makeColorPicker();
+        createEndingButtons();
         Scene myScene = new Scene(layout);
         return myScene;
     }
@@ -31,8 +35,24 @@ public class PenPopup {
     private void makeColorPicker() {
         Label penColorLabel = new Label("Choose your pen color: ");
         ColorPicker myColorPicker = new ColorPicker();
+        myRow.setPadding(new Insets(5, 20, 10, 20));
         myRow.getChildren().addAll(penColorLabel, myColorPicker);
-        layout.getChildren().add(myRow);
+        layout.setTop(myRow);
+    }
+    
+    private void createEndingButtons() {
+        Button applyButton = makeButton("Apply Changes");
+        Button clearButton = makeButton("Clear");
+        HBox testBox = new HBox(SPACING);
+        testBox.setPadding(new Insets(0, 20, 10, 20));
+        testBox.setAlignment(Pos.CENTER_RIGHT);
+        testBox.getChildren().addAll(clearButton, applyButton);
+        layout.setBottom(testBox);
+    }
+    
+    private Button makeButton(String buttonText) {
+        Button addButton = new Button(buttonText);
+        return addButton;
     }
     
 }
