186df6d6d7aa14a3d35c91903ae1e69324a1c872
Kayla Schulz <kms114@duke.edu>
2016-10-31 23:48:55 -0400

    Pen movement very very basic functionality

--- src/front_end/view_modules/turtleBox/turtleMovement/PenMovement.java
+++ src/front_end/view_modules/turtleBox/turtleMovement/PenMovement.java
@@ -3,6 +3,7 @@ package front_end.view_modules.turtleBox.turtleMovement;
 import front_end.view_modules.turtleBox.ITurtleBox;
 import integration.observe.IObserver;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 
 public class PenMovement implements IObserver {
@@ -28,6 +29,9 @@ public class PenMovement implements IObserver {
     protected void drawWithPen(ITurtleBox myTurtleBox) {
         //TODO: Fix turtleBox being null
         gc = myTurtleBox.getGC();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(5);
+        gc.strokeLine(0, 0, 300, 300);
     }
 
 }
