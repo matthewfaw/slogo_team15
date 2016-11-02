db2fbee49a04fa27acb3d18533a80d374e79b52c
Kayla Schulz <kms114@duke.edu>
2016-11-02 03:37:08 -0400

    Fixed animation bugs

--- src/front_end/view_modules/turtleBox/turtleMovement/ImageMover.java
+++ src/front_end/view_modules/turtleBox/turtleMovement/ImageMover.java
@@ -46,10 +46,15 @@ public class ImageMover implements IDrawer {
     public SequentialTransition move () {
         loadImage();
         checkVisibility();
+        System.out.println("xprev " + previousXCoordinate());
+        System.out.println("yprev " + previousYCoordinate());
+        System.out.println("xcur " + currentXCoordinate());
+        System.out.println("ycur " + currentYCoordinate());
         if (previousXCoordinate() == currentXCoordinate() &&
-                previousYCoordinate() == currentYCoordinate()) {
+            previousYCoordinate() == currentYCoordinate()) {
+            System.out.println("Haha you suck");
             return new SequentialTransition(myImageView, rotate());
-            }
+        }
         else {
             return new SequentialTransition(myImageView, translate(), rotate());
         }
@@ -71,17 +76,12 @@ public class ImageMover implements IDrawer {
         Path path = new Path();
         PathTransition pt = new PathTransition();
 
-            MoveTo myMove = new MoveTo(previousXCoordinate(), previousYCoordinate());
-            LineTo myLine = new LineTo(currentXCoordinate(), currentYCoordinate());
-            path.getElements().addAll(myMove);
-            path.getElements().add(myLine);
-            System.out.println("y " + previousYCoordinate());
-            pt = new PathTransition(Duration.millis(distance() * 50), path, myImageView);
+        MoveTo myMove = new MoveTo(previousXCoordinate(), previousYCoordinate());
+        LineTo myLine = new LineTo(currentXCoordinate(), currentYCoordinate());
+        path.getElements().addAll(myMove, myLine);
+        pt = new PathTransition(Duration.millis(800), path, myImageView);
 
-        System.out.println("xprev " + previousXCoordinate());
-        System.out.println("yprev " + previousYCoordinate());
-        System.out.println("xcur " + currentXCoordinate());
-        System.out.println("ycur " + currentYCoordinate());
+
         return pt;
     }
 
@@ -101,16 +101,11 @@ public class ImageMover implements IDrawer {
         return myMoveCalc.translateYCoordinate(myRobot.getCurrentCoordinate().getY());
     }
 
-    private double distance () {
-        return Math.sqrt(Math.pow(currentYCoordinate() - previousYCoordinate(), 2) -
-                         Math.pow(currentXCoordinate() - previousXCoordinate(), 2));
-    }
-
     private RotateTransition rotate () {
         RotateTransition rt = new RotateTransition();
         if (myRobot.getCurrentRotation() != myRobot.getPreviousRotation()) {
             rt = new RotateTransition(Duration.seconds(3));
-            rt.setByAngle(-myRobot.getCurrentRotation()+myRobot.getPreviousRotation());
+            rt.setByAngle(-myRobot.getCurrentRotation() + myRobot.getPreviousRotation());
         }
         else {
             rt = new RotateTransition(Duration.seconds(0));
