066687bf80fe691507b0a95c38753ee0d5362338
George Bernard <george.bernard@duke.edu>
2016-11-05 17:06:32 -0400

    removed redundant variable interface

--- src/front_end/view_modules/IViewVariableAcceptor.java
+++
@@ -1,9 +0,0 @@
-package front_end.view_modules;
-
-import back_end.model.states.IViewableVariableState;
-
-public interface IViewVariableAcceptor {
-
-	public void giveVariableState(IViewableVariableState aViewVariableState);
-	
-}
